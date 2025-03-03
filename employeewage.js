f9585401a46ed15dd1dacb4a7571f400887ecc7b
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-03-03 23:24:17 +0530

    [Sanjay] add. daily hours worked also wage

--- employeewage.js
+++ employeewage.js
@@ -192,3 +192,24 @@ console.log("Full Working Days: "+fullWokringDays);
 console.log("Part Working Days: "+partWokringDays);
 
 console.log("Non Working Days: "+nonWorkingDays);
+
+
+
+
+let empDailyHrsAndWageArr = new Array();
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyHrsAndWageArr.push({
+        dayNum:totalWorkingDays,
+        dailyHours:empHrs,
+        dailyWage: calcDailyWage(empHrs),
+        toString(){
+            return '\nDay ' + this.dayNum + ' => Working Hours is ' + this.dailyHours + ' And Wage Earned = ' + this.dailyWage;
+        },
+    });
+}
+console.log("UC 10 showing daily hours worked and wage earned: "+empDailyHrsAndWageArr)
+empDailyHrsAndWageArr.forEach(empDay => console.log(empDay.toString()));
