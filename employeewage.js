75a5737e2ab152b1dcce2d1e979092a7477aa5ca
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-03-03 22:03:25 +0530

    [Sanjay] add. store daily along with total wage

--- employeewage.js
+++ employeewage.js
@@ -30,18 +30,26 @@ function getWorkingHours(empcheck){
     }
 }
 
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
 
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
 let empHrs = 0;
-const MAX_HRS_IN_MONTH = 100;
-const NUM_OF_WORKING_DAYS = 10;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 
-while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+let empDailyWageArr = new Array();
+
+while(totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
-    let empcheck = Math.floor(Math.random() * 10 ) % 3;
-    totalEmpHrs += getWorkingHours(empcheck)
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5 - Total Days: "+totalEmpHrs+" Emp Wage: "+empWage)
+
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: "+ totalWorkingDays+" Total Hrs: "+totalEmpHrs +" Emp Wage: "+empWage)
