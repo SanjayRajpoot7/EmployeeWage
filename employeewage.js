4f5ae15975fe9a3c3f2e3ead32e3cb5e697f4bda
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-03-03 16:15:17 +0530

    [Sanjay] add. check whether employee present or not

---
+++ employeewage.js
@@ -0,0 +1,12 @@
+
+const randomValue = Math.random();
+let check;
+
+if (randomValue >= 0 && randomValue <= 0.5) {
+    check = "Employee is Absent"; 
+} else {
+    check = "Employee is Present"; 
+}
+
+console.log(check)
+
