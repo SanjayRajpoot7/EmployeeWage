d3819472bc9028d681fafa302e9a2ed0fa08c3ea
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-03-04 11:37:06 +0530

    [Sanjay] if any case username is not valid it will throw exception

--- employeewage.js
+++ employeewage.js
@@ -44,140 +44,6 @@ let empDailyWageArr = new Array();
 let empDailyHrsAndWageArr = new Array();
 let empDailyHrsMap = new Map();
 
-// while(totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
-//     totalWorkingDays++;
-//     let empCheck = Math.floor(Math.random() * 10) % 3;
-//     let empHrs = getWorkingHours(empCheck);
-//     totalEmpHrs += empHrs;
-//     empDailyWageArr.push(calcDailyWage(empHrs));
-// }
-
-
-// let empWage = calcDailyWage(totalEmpHrs);
-// console.log("UC6 - Total Days: "+ totalWorkingDays+" Total Hrs: "+totalEmpHrs +" Emp Wage: "+empWage)
-
-
-
-
-
-// // 7A 
-// let totEmpWage = 0;
-// function sum(dailyWage){
-//     totEmpWage += dailyWage;
-// }
-
-// empDailyWageArr.forEach(sum);
-// console.log("UC7A - Total Days: "+ totalWorkingDays+" Total Hrs: "+totalEmpHrs +" Emp Wage: "+totEmpWage)
-
-// function totalWages(totalWage, dailyWage) {
-//     return totalWage + dailyWage;
-// }
-// console.log("UC7A - Emp Wage with reduce: "+ empDailyWageArr.reduce(totalWages, 0));
-
-
-// // 7B 
-// let dailyCntr = 0;
-// function mapDailyWithDaily(dailyWage){
-//     dailyCntr++;
-//     return dailyCntr + " " + dailyWage;
-// }
-
-// let mapDayWithWageArr = empDailyWageArr.map(mapDailyWithDaily);
-// console.log("UC7B - Daily Wage Map");
-// console.log(mapDayWithWageArr);
-
-
-
-// // 7C 
-// function fullTimeWage(dailyWage) {
-//     return dailyWage.includes("160");
-// }
-
-// let fullDayWageArr = mapDayWithWageArr.filter(fullTimeWage);
-// console.log("UC7C - Daily Wage Filter When FullTime Wage Earned");
-// console.log(fullDayWageArr);
-
-
-
-// // 7D
-// function findFullTimeWage(dailyWage) {
-//     return dailyWage.includes("160");
-// }
-// console.log("UC7D - First time FullTime wage was earned on day: "+mapDayWithWageArr.find(findFullTimeWage))
-
-
-// // 7E
-
-// function isAllFullTimeWage(dailyWage){
-//     return dailyWage.includes("160");
-// }
-
-// console.log("UC 7E - Check All time element have full time wage: "+fullDayWageArr.every(isAllFullTimeWage));
-
-
-// // UC 7F
-// function isAnyPartTimeWage(dailyWage){
-//     return dailyWage.includes("80");
-// }
-
-// console.log("UC 7F - Check if any part time Wage: "+mapDayWithWageArr.some(isAnyPartTimeWage));
-
-
-
-// // UC 7G 0 find the number of days the employee worked
-// function totalDaysWorked(numOfDays, dailyWage){
-//     if(dailyWage > 0)
-//         return numOfDays + 1;
-     
-//     return numOfDays;
-// }
-
-// console.log("UC 7G - Number of days emp worked: "+empDailyWageArr.reduce(totalDaysWorked, 0));
-
-// let empDailyWageMap = new Map();
-
-// function calcDailyWage(empHrs){
-//     return empHrs * WAGE_PER_HOUR;
-// }
-
-// while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-//     totalWorkingDays++;
-//     let empCheck = Math.floor(Math.random() * 10) % 3;
-//     let empHrs = getWorkingHours(empCheck);
-//     totalEmpHrs += empHrs;
-//     empDailyWageArr.push(calcDailyWage(empHrs));
-//     empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
-// }
-
-// console.log(empDailyWageMap);
-
-// function totalWages(totalWage, dailyWage){
-//     return totalWage + dailyWage;
-// }
-
-// console.log("UC 8 - Emp wage map totalHrs: "+ Array.from(empDailyWageMap.values()).reduce(totalWages,0));
-
-
-
-
-
-// let empDailyHrsMap = new Map(); // Create 
-// empDailyHrsMap.set(1, 8);  
-// empDailyHrsMap.set(2, 4);
-// empDailyHrsMap.set(3, 0); 
-// empDailyHrsMap.set(4, 8);
-// empDailyHrsMap.set(5, 4); 
-
-// // Define your daily wage array
-// const findTotal = (totalVal, dailyVal) => {
-//     return totalVal + dailyVal;
-// }
-
-// let count = 0;
-// let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
-// let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
-// console.log("UC 9A - Emp Wage with arrow: "+findTotal()+ " Total Hours: "+totalHours+" Total Wages: "+totalSalary);
-
 
 let nonWorkingDays = new Array();
 let partWokringDays = new Array();
@@ -235,77 +101,93 @@ let nonWorkingDayNums = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrs
 console.log("\nUC 11D Non Working Day: "+nonWorkingDayNums);
 
 
-class EmployeeWageData{
+// Employee wage Data class
+
+class EmployeeWageData {
+    // Private variables
     id;
     name;
     salary;
     gender;
     startDate;
-    
 
     constructor(...params) {
-        this.id = params[0]
-        this.name = params[1]
-        this.salary = params[2]
-        this.gender = params[3]
-        this.startDate = params[4]
+        this.id = params[0]; 
+        this.name = params[1];  
+        this.salary = params[2]; 
+        this.gender = params[3]; 
+        this.startDate = params[4];  
     }
 
-    get name(){
+    get name() {
         return this.name;
     }
 
-    
-    set name(name){
-        this.name = name;
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name)) {
+            this.name = name;
+        } else {
+            throw "Name is Incorrect!";
+        }
     }
 
-    get salary(){
+    get salary() {
         return this.salary;
     }
-    set salary(salary){
-        this.salary = salary;
+
+    set salary(salary) {
+        this.salary = salary
     }
 
-    get id(){
+    get id() {
         return this.id;
     }
-    set id(id){
+
+    set id(id) {
         this.id = id;
     }
-    
-    get gender(){
+
+    get gender() {
         return this.gender;
     }
 
-    set gender(gender){
+    set gender(gender) {
         this.gender = gender;
     }
 
-    get startDate(){
+    get startDate() {
         return this.startDate;
     }
 
-    set startDate(startDate){
-        return this.startDate = startDate
+    set startDate(startDate) {
+       this.startDate = startDate;
     }
 
-
-
-    toString(){
-        const options = { year : 'numeric', month : 'long', day : 'numeric' };
-
-        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
-
-        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary+ ", gender = " + this.gender + ", startDate = " + empDate;
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate;
     }
-
 }
 
-let employeeWageData = new EmployeeWageData(1,"Raj",45000)
-console.log(employeeWageData.toString());
-employeeWageData.name = "Sanjay";
-console.log(employeeWageData.toString());
+try {
+    let employeeWageData = new EmployeeWageData(1, "aaj", 45000, "M", new Date()); 
+    console.log(employeeWageData.toString());
+} catch (e) {
+    console.log(e); 
+}
 
-let newEmployeeWageData = new EmployeeWageData(3,"Jay",65000,"M",new Date());
-console.log(newEmployeeWageData.toString());
+try {
+    let employeeWageData = new EmployeeWageData(2, "Sanjay", 45000, "M", new Date()); 
+    console.log(employeeWageData.toString()); 
+} catch (e) {
+    console.log(e);
+}
+
+try {
+    let newEmployeeWageData = new EmployeeWageData(3, "Raju", 65000, "M", new Date());
+    console.log(newEmployeeWageData.toString());
+} catch (e) {
+    console.log(e);
+}
