9d254a177efecdadf0172469187f604e8b864abf
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-03-04 11:43:31 +0530

    [Sanjay] add. handled exception when invalid data

--- employeewage.js
+++ employeewage.js
@@ -105,81 +105,97 @@ console.log("\nUC 11D Non Working Day: "+nonWorkingDayNums);
 
 class EmployeeWageData {
     // Private variables
-    id;
-    name;
-    salary;
-    gender;
-    startDate;
+    _id;
+    _name;
+    _salary;
+    _gender;
+    _startDate;
 
     constructor(...params) {
-        this.id = params[0]; 
-        this.name = params[1];  
-        this.salary = params[2]; 
-        this.gender = params[3]; 
-        this.startDate = params[4];  
+        this.id = params[0];  // Using setter for validation
+        this.name = params[1];  // Using setter for validation
+        this.salary = params[2];  // Using setter for validation
+        this.gender = params[3];  // Using setter for validation
+        this.startDate = params[4];  // Using setter for validation
     }
 
     get name() {
-        return this.name;
+        return this._name;
     }
 
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
         if (nameRegex.test(name)) {
-            this.name = name;
+            this._name = name;
         } else {
             throw "Name is Incorrect!";
         }
     }
 
     get salary() {
-        return this.salary;
+        return this._salary;
     }
 
     set salary(salary) {
-        this.salary = salary
+        if (salary > 0) {
+            this._salary = salary;
+        } else {
+            throw "Salary must be positive!";
+        }
     }
 
     get id() {
-        return this.id;
+        return this._id;
     }
 
     set id(id) {
-        this.id = id;
+        if (id > 0) {
+            this._id = id;
+        } else {
+            throw "ID must be positive!";
+        }
     }
 
     get gender() {
-        return this.gender;
+        return this._gender;
     }
 
     set gender(gender) {
-        this.gender = gender;
+        if (gender === "M" || gender === "F") {
+            this._gender = gender;
+        } else {
+            throw "Gender must be 'M' or 'F'!";
+        }
     }
 
     get startDate() {
-        return this.startDate;
+        return this._startDate;
     }
 
     set startDate(startDate) {
-       this.startDate = startDate;
+        if (startDate instanceof Date) {
+            this._startDate = startDate;
+        } else {
+            throw "Start date must be a valid date!";
+        }
     }
 
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
-        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate;
+        const empDate = !this._startDate ? "undefined" : this._startDate.toLocaleDateString("en-US", options);
+        return "id = " + this._id + ", name = " + this._name + ", salary = " + this._salary + ", gender = " + this._gender + ", startDate = " + empDate;
     }
 }
 
 try {
-    let employeeWageData = new EmployeeWageData(1, "aaj", 45000, "M", new Date()); 
+    let employeeWageData = new EmployeeWageData(1, "aaj", 45000, "M", new Date());  // Invalid name
     console.log(employeeWageData.toString());
 } catch (e) {
-    console.log(e); 
+    console.log(e);  // This should print "Name is Incorrect!"
 }
 
 try {
-    let employeeWageData = new EmployeeWageData(2, "Sanjay", 45000, "M", new Date()); 
+    let employeeWageData = new EmployeeWageData(2, "Sanjay", 45000, "S", new Date());  
     console.log(employeeWageData.toString()); 
 } catch (e) {
     console.log(e);
